22da537bd82fd98a2e036d3fb54c3dcacdb78638
Cindy Chen <80414635+dchen-win@users.noreply.github.com>
2025-05-31 18:45:28 -0700

    Make the changes

--- Python HW/Assignment 4/Draft/import re.py
+++
@@ -1,6 +0,0 @@
-import re
-s = "<html><head><title>Title</title>"
-string_val_new = s.split()
-print(string_val_new)
-# print(re.match('<.*>', s).span())
-# print(re.match('<.*?>', string_val_new).group())
